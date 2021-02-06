ba31906c95f00ae3a69a4cd192aeead156379cef
Andre Parsons <andrparsons@gmail.com>
2021-02-06 10:28:50 -0500

    created plan page placeholder component

---
+++ pages/plan.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Plan() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>coffeeroasters | About Us</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <main className={styles.main}>
+        Plan page placeholder
+      </main>
+    </div>
+  );
+}
