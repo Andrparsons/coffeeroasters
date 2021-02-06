27d6797e960f570ddee9cb1fc32b906ab2adb2ae
Andre Parsons <andrparsons@gmail.com>
2021-02-05 23:26:17 -0500

    added index page content

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,120 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>coffeeroasters | Home</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.intro}>
+          <h1>Great coffee made simple.</h1>
+          <p>
+            Start your mornings with the world’s best coffees. Try our expertly
+            curated artisan coffees from our best roasters delivered directly to
+            your door, at your schedule.
+          </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <button>Create your plan</button>
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+        <div className={styles.collection}>
+          <h2>Our collection</h2>
+          <div className={styles.collectionGroup}>
+            <h3>Gran Espresso</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Light and flavorful blend with cocoa and black pepper for an
+              intense experience
             </p>
-          </a>
+
+            <h3>Planalto</h3>
+            <p>
+              Brazilian dark roast with rich and velvety body, and hints of
+              fruits and nuts
+            </p>
+
+            <h3>Piccollo</h3>
+            <p>
+              Mild and smooth blend featuring notes of toasted almond and dried
+              cherry
+            </p>
+
+            <h3>Danche</h3>
+            <p>
+              Ethiopian hand-harvested blend densely packed with vibrant fruit
+              notes
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.features}>
+          <h2>Why choose us?</h2>
+
+          <p>
+            A large part of our role is choosing which particular coffees will
+            be featured in our range. This means working closely with the best
+            coffee growers to give you a more impactful experience on every
+            level.
+          </p>
+
+          <h3>Best quality</h3>
+          <p>
+            Discover an endless variety of the world’s best artisan coffee from
+            each of our roasters.
+          </p>
+
+          <h3>Exclusive benefits</h3>
+          <p>
+            Special offers and swag when you subscribe, including 30% off your
+            first shipment.
+          </p>
+
+          <h3>Free shipping</h3>
+          <p>
+            We cover the cost and coffee is delivered fast. Peak freshness:
+            guaranteed.
+          </p>
+        </div>
+
+        <div className={styles.steps}>
+          <h3>How it works</h3>
+
+          <div className={styles.step}>
+            <h4>01</h4>
+            <h5>Pick your coffee</h5>
+            <p>
+              Select from our evolving range of artisan coffees. Our beans are
+              ethically sourced and we pay fair prices for them. There are new
+              coffees in all profiles every month for you to try out.
+            </p>
+          </div>
+
+          <div className={styles.step}>
+            <h4>02</h4>
+            <h5>Choose the frequency</h5>
+            <p>
+              Customize your order frequency, quantity, even your roast style
+              and grind type. Pause, skip or cancel your subscription with no
+              commitment through our online portal.
+            </p>
+          </div>
+
+          <div className={styles.step}>
+            <h4>03</h4>
+            <h5>Receive and enjoy!</h5>
+            <p>
+              We ship your package within 48 hours, freshly roasted. Sit back
+              and enjoy award-winning world-class coffees curated to provide a
+              distinct tasting experience.
+            </p>
+          </div>
+          <button>Create your plan</button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
