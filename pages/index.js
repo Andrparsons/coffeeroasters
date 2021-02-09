6ff2c6239a3527dcd46f6014c94327a45cdd179b
Andre Parsons <andrparsons@gmail.com>
2021-02-09 08:33:38 -0500

    refactored sections in index to remove repeat code

--- pages/index.js
+++ pages/index.js
@@ -135,39 +135,40 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.steps}>
+        <div className={styles.stepSection}>
           <h3>How it works</h3>
+          <div className={styles.steps}>
+            <div className={styles.step}>
+              <h4>01</h4>
+              <h5>Pick your coffee</h5>
+              <p>
+                Select from our evolving range of artisan coffees. Our beans are
+                ethically sourced and we pay fair prices for them. There are new
+                coffees in all profiles every month for you to try out.
+              </p>
+            </div>
 
-          <div className={styles.step}>
-            <h4>01</h4>
-            <h5>Pick your coffee</h5>
-            <p>
-              Select from our evolving range of artisan coffees. Our beans are
-              ethically sourced and we pay fair prices for them. There are new
-              coffees in all profiles every month for you to try out.
-            </p>
-          </div>
+            <div className={styles.step}>
+              <h4>02</h4>
+              <h5>Choose the frequency</h5>
+              <p>
+                Customize your order frequency, quantity, even your roast style
+                and grind type. Pause, skip or cancel your subscription with no
+                commitment through our online portal.
+              </p>
+            </div>
 
-          <div className={styles.step}>
-            <h4>02</h4>
-            <h5>Choose the frequency</h5>
-            <p>
-              Customize your order frequency, quantity, even your roast style
-              and grind type. Pause, skip or cancel your subscription with no
-              commitment through our online portal.
-            </p>
+            <div className={styles.step}>
+              <h4>03</h4>
+              <h5>Receive and enjoy!</h5>
+              <p>
+                We ship your package within 48 hours, freshly roasted. Sit back
+                and enjoy award-winning world-class coffees curated to provide a
+                distinct tasting experience.
+              </p>
+            </div>
+            <Button>Create your plan</Button>
           </div>
-
-          <div className={styles.step}>
-            <h4>03</h4>
-            <h5>Receive and enjoy!</h5>
-            <p>
-              We ship your package within 48 hours, freshly roasted. Sit back
-              and enjoy award-winning world-class coffees curated to provide a
-              distinct tasting experience.
-            </p>
-          </div>
-          <Button>Create your plan</Button>
         </div>
       </main>
     </div>
