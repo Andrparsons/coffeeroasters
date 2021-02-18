eaa9c6f24c05d64e966bb9097219d45d1ec737a2
Andre Parsons <andrparsons@gmail.com>
2021-02-18 07:28:58 -0500

    separated opitions on plan page

--- pages/plan.js
+++ pages/plan.js
@@ -68,45 +68,48 @@ export default function Plan() {
           </div>
         </section>
 
-        <div className={styles.sidebar}>
-          01 Preferences 02 Bean type 03 Quantity 04 Grind option 05 Deliveries
-        </div>
-
-        <div className={styles.options}>
-          <div className={styles.option}>
-            How do you drink your coffee? Capsule Compatible with Nespresso
-            systems and similar brewers Filter For pour over or drip methods
-            like Aeropress, Chemex, and V60 Espresso Dense and finely ground
-            beans for an intense, flavorful experience
+        <section className={styles.optionSection}>
+          <div className={styles.optionStatus}>
+            01 Preferences 02 Bean type 03 Quantity 04 Grind option 05
+            Deliveries
           </div>
 
-          <div className={styles.option}>
-            What type of coffee? Single origin Distinct, high quality coffee
-            from a specific family-owned farm Decaf Just like regular coffee,
-            except the caffeine has been removed Blended Combination of two or
-            three dark roasted beans of organic coffees
-          </div>
+          <div className={styles.options}>
+            <div className={styles.option}>
+              How do you drink your coffee? Capsule Compatible with Nespresso
+              systems and similar brewers Filter For pour over or drip methods
+              like Aeropress, Chemex, and V60 Espresso Dense and finely ground
+              beans for an intense, flavorful experience
+            </div>
 
-          <div className={styles.option}>
-            How much would you like? 250g Perfect for the solo drinker. Yields
-            about 12 delicious cups. 500g Perfect option for a couple. Yields
-            about 40 delectable cups. 1000g Perfect for offices and events.
-            Yields about 90 delightful cups.
-          </div>
-          <div className={styles.option}>
-            Want us to grind them? Wholebean Best choice if you cherish the full
-            sensory experience Filter For drip or pour-over coffee methods such
-            as V60 or Aeropress Cafetiére Course ground beans specially suited
-            for french press coffee
-          </div>
+            <div className={styles.option}>
+              What type of coffee? Single origin Distinct, high quality coffee
+              from a specific family-owned farm Decaf Just like regular coffee,
+              except the caffeine has been removed Blended Combination of two or
+              three dark roasted beans of organic coffees
+            </div>
 
-          <div className={styles.option}>
-            How often should we deliver? Every week $14.00 per shipment.
-            Includes free first-class shipping. Every 2 weeks $17.25 per
-            shipment. Includes free priority shipping. Every month $22.50 per
-            shipment. Includes free priority shipping.
+            <div className={styles.option}>
+              How much would you like? 250g Perfect for the solo drinker. Yields
+              about 12 delicious cups. 500g Perfect option for a couple. Yields
+              about 40 delectable cups. 1000g Perfect for offices and events.
+              Yields about 90 delightful cups.
+            </div>
+            <div className={styles.option}>
+              Want us to grind them? Wholebean Best choice if you cherish the
+              full sensory experience Filter For drip or pour-over coffee
+              methods such as V60 or Aeropress Cafetiére Course ground beans
+              specially suited for french press coffee
+            </div>
+
+            <div className={styles.option}>
+              How often should we deliver? Every week $14.00 per shipment.
+              Includes free first-class shipping. Every 2 weeks $17.25 per
+              shipment. Includes free priority shipping. Every month $22.50 per
+              shipment. Includes free priority shipping.
+            </div>
           </div>
-        </div>
+        </section>
 
         <div className={styles.summary}>
           Order Summary “I drink coffee _____, with a _____ type of bean. _____
